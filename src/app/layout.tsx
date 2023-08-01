68edeca1d946f1695633a3226926f90f46528fde
DosSantosDev2022 <dossantosdevoficial@gmail.com>
2023-07-31 23:33:52 -0300

    add components header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import { NextAuthProvider } from '@/providers/auth'
 import './globals.css'
 import type { Metadata } from 'next'
 import { Poppins } from 'next/font/google'
+import Header from './components/Header'
 
 const poppins = Poppins({ subsets: ['latin'], weight:[
       '400','500','600','700'
@@ -21,6 +22,7 @@ export default function RootLayout({
     <html lang="pt-br">
       <body className={poppins.className}>
         <NextAuthProvider>
+          <Header/>
           {children}
         </NextAuthProvider>
       </body>
