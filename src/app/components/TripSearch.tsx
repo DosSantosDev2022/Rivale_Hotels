202773342d7175d7a43d405b1338137c99f86a73
DosSantosDev2022 <dossantosdevoficial@gmail.com>
2023-08-01 23:39:28 -0300

    fix: mudando name componets

--- src/app/components/TripSearch.tsx
+++ src/app/components/TripSearch.tsx
@@ -3,11 +3,12 @@
 import React from "react";
 import Input from "@/components/input";
 import DatePicker from "@/components/DatePicker";
-import CurrencyInput from "react-currency-input-field";
+import CurrencyInput from "@/components/CurrencyInput";
+import Button from "@/components/Button";
 
 const TripSearch = () => {
   return(
-    <div className="container mx-auto p-5 ">
+    <div className="container mx-auto p-5 bg-search-background bg-cover bg-center bg-no-repeat ">
         <h1 className="font-semibold text-xl text-center text-color02">Encontre sua próxima <span className="text-color01">viagem !</span></h1>
 
         <div className="flex flex-col gap-4 mt-5">
@@ -17,6 +18,7 @@ const TripSearch = () => {
           <DatePicker placeholderText="Data de Ida" className="w-full" onChange={() =>{}}/>
           <CurrencyInput placeholder="Orçamento" className="w-full" />
           </div>
+          <Button>Pesquisar</Button>
         </div>
     </div>
   )
