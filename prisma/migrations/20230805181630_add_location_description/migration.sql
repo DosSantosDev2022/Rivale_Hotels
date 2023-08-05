0871bdf4b5cc5ba072948d6c5518bad7a4c0b4ec
DosSantosDev2022 <dossantosdevoficial@gmail.com>
2023-08-05 15:45:08 -0300

    feat: add column schema prisma locationDescription

---
+++ prisma/migrations/20230805181630_add_location_description/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Trip" ADD COLUMN     "locationDescription" TEXT NOT NULL DEFAULT 'Lorem Ipsum é simplesmente um texto fictício da indústria tipográfica e de impressão. Lorem Ipsum tem sido o texto fictício padrão da indústria desde os anos 1500, quando um impressor desconhecido pegou uma galera de tipos e os embaralhou para fazer um livro de espécimes de tipos. Ele sobreviveu não apenas a cinco séculos, mas também ao salto para a composição eletrônica, permanecendo essencialmente inalterado';
