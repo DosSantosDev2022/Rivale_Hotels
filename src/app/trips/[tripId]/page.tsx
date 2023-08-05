3a952cc214885f880fc3934a783bb8ab0437ecc4
DosSantosDev2022 <dossantosdevoficial@gmail.com>
2023-08-05 11:51:13 -0300

    feat: add components and page

---
+++ src/app/trips/[tripId]/page.tsx
@@ -0,0 +1,29 @@
+import { prisma } from "@/lib/prisma";
+import React from "react";
+import TripHeader from "./components/TripHeader"
+
+
+const getTripDedails = async (tripId: string) => {
+  const trip = await prisma.trip.findUnique({
+    where: {
+      id: tripId
+    }
+  })
+  return trip;
+}
+
+const TripDetails = async ({params}:{params: {tripId:string}}) => {
+  const trip = await getTripDedails (params.tripId);
+
+  if (!trip) return null;
+
+  return (
+    <div className="container mx-auto">
+      <TripHeader trip={trip}/>
+      {/* reservas  */}
+        
+    </div>
+  )
+}
+
+export default TripDetails
