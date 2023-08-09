8e0f7f60b1316f8d6029970f70321c837c657126
DosSantosDev2022 <dossantosdevoficial@gmail.com>
2023-08-08 21:41:11 -0300

    fix: corrigindo route ts

--- src/app/api/trips/check/route.ts
+++ src/app/api/trips/check/route.ts
@@ -2,73 +2,80 @@ import { prisma } from "@/lib/prisma";
 import { differenceInDays, isBefore } from "date-fns";
 import { NextResponse } from "next/server";
 
+export async function POST(request: Request) {
+  const req = await request.json();
 
-export async function POST(request:Request){
-    const req = await request.json();
+  const trip = await prisma.trip.findUnique({
+    where: {
+      id: req.tripId,
+    },
+  });
 
-    const trip = await prisma.trip.findUnique({
-      where:{
-        id: req.tripId
-      }
-    });
-
-    if(!trip){
+  if (!trip) {
     return new NextResponse(
       JSON.stringify({
-        error:{
+        error: {
           code: "TRIP_NOT_FOUND",
         },
       })
-    )}
+    );
+  }
 
-    if(isBefore(new Date(req.startDate),new Date(trip.startDate))) {
-      
-      return new NextResponse(
-        JSON.stringify({
-          error:{
-            code: "INVALID-START_DATE",
-          },
-        }),
-        {
-          status:400,
-        }
-      )
-    }
-
-    if(isBefore(new Date(trip.endDate), new Date(req.endDate))){
-      return new NextResponse(
-        JSON.stringify({
-          error:{
-            code: "INVALID_END_DATE",
-          },
-        }),
-      );
-    }
-
-    const reservation = await prisma.tripReservation.findMany({
-      where: {
-        trip: req.tripId,
-         // VERIFICA SE EXISTE RESERVA ENTRE AS DATAS
-        startDate: {
-          lte: new Date(req.endDate)
+  if (isBefore(new Date(req.startDate), new Date(trip.startDate))) {
+    return new NextResponse(
+      JSON.stringify({
+        error: {
+          code: "INVALID_START_DATE",
         },
-        endDate:{
-          gte: new Date(req.startDate)
-        }
+      }),
+      {
+        status: 400,
       }
+    );
+  }
+
+  // Data de fim recebida precisa ser menor ou igual a data de fim da viagem
+  if (isBefore(new Date(trip.endDate), new Date(req.endDate))) {
+    return new NextResponse(
+      JSON.stringify({
+        error: {
+          code: "INVALID_END_DATE",
+        },
+      }),
+      {
+        status: 400,
+      }
+    );
+  }
+
+  const reservations = await prisma.tripReservation.findMany({
+    where: {
+      tripId: req.tripId,
+      // VERIFICA SE EXISTE RESERVA ENTRE AS DATAS
+      startDate: {
+        lte: new Date(req.endDate),
+      },
+      endDate: {
+        gte: new Date(req.startDate),
+      },
+    },
+  });
+
+  if (reservations.length > 0) {
+    return new NextResponse(
+      JSON.stringify({
+        error: {
+          code: "TRIP_ALREADY_RESERVED",
+        },
+      })
+    );
+  }
+
+  return new NextResponse(
+    JSON.stringify({
+      success: true,
+      trip,
+      totalPrice: differenceInDays(new Date(req.endDate), new Date(req.startDate)) * Number(trip.pricePerDay),
     })
-
-    if (reservation.length > 0){
-        return new NextResponse(JSON.stringify({
-          error:{
-            Code: 'TRIP_ALREADY_RESERVED'
-          }
-        }))
-    }
-
-    return new NextResponse(JSON.stringify({
-        success:true,
-        trip,
-        totalPrice: differenceInDays(new Date(req.endDate), new Date(req.startDate)) * Number(trip.pricePerDay),
-    }))
+  );
 }
