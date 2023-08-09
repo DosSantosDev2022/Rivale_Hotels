5c6247b71968237a944e8e753c29d7244e311182
DosSantosDev2022 <dossantosdevoficial@gmail.com>
2023-08-09 00:57:13 -0300

    feat: creat page myreservations

--- src/app/my-trips/page.tsx
+++ src/app/my-trips/page.tsx
@@ -1,39 +1,61 @@
-'use client'
-import { TripReservation } from "@prisma/client";
+"use client";
+
+import React, { useEffect, useState } from "react";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
-import React, { useEffect, useState } from "react";
+import { Prisma } from "@prisma/client";
+import Link from "next/link";
 
+import UserReservationItem from "./components/UserReservationItem";
+import Button from "@/components/Button";
 
+const MyTrips = () => {
+  const [reservations, setReservations] = useState<
+    Prisma.TripReservationGetPayload<{
+      include: { trip: true };
+    }>[]
+  >([]);
 
-  const MyTrips =   () => {
-    const [reservations, setReservations] = useState<TripReservation[]>([])
-  const {status, data} = useSession();
+  const { status, data } = useSession();
 
   const router = useRouter();
 
+  const fetchReservations = async () => {
+    const response = await fetch(`http:localhost:3000/api/user/${(data?.user as any)?.id}/reservation`);
 
-useEffect(() =>{
-    if(status ===  'unauthenticated' || !data?.user){
-      return router.push('/')
+    const json = await response.json();
+
+    setReservations(json);
+  };
+
+  useEffect(() => {
+    if (status === "unauthenticated") {
+      return router.push("/");
     }
 
-    const fetchReservations = async () => {
-      const response = await fetch(`http://localhost:3000/api/user/${(data?.user as any).id}/reservations`);
-      const json = await response.json();
-  
-      setReservations(json)
-    }
-
-    fetchReservations()
-},[data?.user, router, status])
-
+    fetchReservations();
+  }, [status]);
 
   return (
-    <div>
+    <div className="container mx-auto p-5">
+      <h1 className="font-semibold text-color02 text-xl lg:mb-5">Minhas Viagens</h1>
+      {reservations.length > 0 ? (
+        <div className="flex flex-col lg:grid lg:grid-cols-3 lg:gap-14">
+          {reservations?.map((reservation) => (
+            <UserReservationItem fetchReservations={fetchReservations} key={reservation.id} reservation={reservation} />
+          ))}
+        </div>
+      ) : (
+        <div className="flex flex-col lg:max-w-[500px]">
+          <p className="mt-2 font-medium text-color02">Você ainda não tem nenhuma reserva!</p>
 
+          <Link href="/">
+            <Button className="w-full mt-2 lg:mt-5">Fazer reserva</Button>
+          </Link>
+        </div>
+      )}
     </div>
-    );
+  );
 };
 
 export default MyTrips;
