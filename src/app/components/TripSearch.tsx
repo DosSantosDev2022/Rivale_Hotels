788f73159c37371d49b741df7369cf1f69cb84c4
DosSantosDev2022 <dossantosdevoficial@gmail.com>
2023-08-14 20:31:04 -0300

    feat: creat search query

--- src/app/components/TripSearch.tsx
+++ src/app/components/TripSearch.tsx
@@ -1,27 +1,95 @@
-'use client';
+"use client";
 
 import React from "react";
 import Input from "@/components/input";
 import DatePicker from "@/components/DatePicker";
 import CurrencyInput from "@/components/CurrencyInput";
 import Button from "@/components/Button";
+import { Controller, useForm } from "react-hook-form";
+import { useRouter } from "next/navigation";
 
-const TripSearch = () => {
-  return(
-    <div className="container mx-auto p-5 bg-search-background bg-cover bg-center bg-no-repeat ">
-        <h1 className="font-semibold text-xl text-center text-color02">Encontre sua próxima <span className="text-color01">viagem !</span></h1>
-
-        <div className="flex flex-col gap-4 mt-5">
-          <Input placeholder="Onde você quer ir ?"/>
-
-          <div className="flex gap-4">
-          <DatePicker placeholderText="Data de Ida" className="w-full" onChange={() =>{}}/>
-          <CurrencyInput placeholder="Orçamento" className="w-full" />
-          </div>
-          <Button>Pesquisar</Button>
-        </div>
-    </div>
-  )
+interface TripSearchForm {
+  text: string;
+  startDate: Date | null;
+  budget: string;
 }
 
-export default TripSearch
+const TripSearch = () => {
+  const router = useRouter();
+
+  const {
+    control,
+    formState: { errors },
+    register,
+    handleSubmit,
+  } = useForm<TripSearchForm>();
+
+  const onSubmit = (data: TripSearchForm) => {
+    router.push(
+      `/trips/search?text=${
+        data.text
+      }&startDate=${data.startDate?.toISOString()}&budget=${data.budget}`
+    );
+  };
+
+  return (
+    <div className="container mx-auto p-5 bg-search-background bg-cover bg-center bg-no-repeat lg:py-28">
+      <h1 className="font-semibold text-2xl text-primaryDarker text-center lg:text-[2.5rem]">
+        Encontre sua próxima <span className="text-primary">viagem!</span>
+      </h1>
+
+      <div className="flex flex-col gap-4 mt-5 lg:flex-row lg:max-w-[948px] lg:mx-auto lg:p-4 lg:bg-primary lg:mt-12 lg:bg-opacity-20 lg:rounded-lg">
+        <Input
+          placeholder="Onde você quer ir?"
+          error={!!errors.text}
+          errorMessage={errors.text?.message}
+          {...register("text", {
+            required: {
+              value: true,
+              message: "Texto é obrigatório.",
+            },
+          })}
+        />
+
+        <div className="flex gap-4 lg:w-full">
+          <Controller
+            name="startDate"
+            control={control}
+            render={({ field }) => (
+              <DatePicker
+                onChange={field.onChange}
+                selected={field.value}
+                placeholderText="Data Final"
+                className="w-full"
+                minDate={new Date()}
+              />
+            )}
+          />
+
+          <Controller
+            name="budget"
+            control={control}
+            render={({ field }) => (
+              <CurrencyInput
+                allowDecimals={false}
+                placeholder="Orçamento"
+                onValueChange={field.onChange as any}
+                value={field.value}
+                onBlur={field.onBlur}
+              />
+            )}
+          />
+        </div>
+
+        <Button
+          onClick={() => handleSubmit(onSubmit)()}
+          className="w-1/2 lg:h-fit"
+        >
+          Buscar
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default TripSearch;
---
+++ src/app/trips/search/page.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+import TripItem from "@/components/TripItem";
+import { Trip } from "@prisma/client";
+import { useSearchParams } from "next/navigation";
+import React, { useEffect } from "react";
+
+const Trips = () => {
+  const [trips, setTrips] = React.useState<Trip[]>([]);
+
+  const searchParams = useSearchParams();
+
+  useEffect(() => {
+    const fetchTrips = async () => {
+      const response = await fetch(
+        `http://localhost:3000/api/trips/search?text=${
+          searchParams.get("text") ?? ""
+        }&startDate=${searchParams.get("startDate")}&budget=${searchParams.get(
+          "budget"
+        )}`
+      );
+
+      const data = await response.json();
+
+      setTrips(data);
+    };
+
+    fetchTrips();
+  }, [searchParams]);
+
+  return (
+    <div className="container mx-auto flex flex-col items-center lg:items-start p-5 lg:pt-10">
+      <h1 className="text-color02 font-semibold text-xl lg:w-full lg:text-left lg:text-[2.5rem]">
+        Viagens Encontradas
+      </h1>
+      <h2 className="text-color03 font-medium mb-5 lg:mt-6 lg:w-full lg:text-left">
+        {trips.length > 0
+          ? "Listamos as melhores viagens pra você!"
+          : "Não encontramos nada nos seus parâmetros! =("}
+      </h2>
+
+      <div className="flex flex-col gap-4 lg:grid lg:grid-cols-4 lg:gap-10 lg:mt-6 lg:pb-16">
+        {trips?.map((trip) => (
+          <TripItem key={trip.id} trip={trip} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+
+export default Trips;
