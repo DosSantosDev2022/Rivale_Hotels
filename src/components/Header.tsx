702772b65a83081282745081c4e89564e1d01e41
DosSantosDev2022 <dossantosdevoficial@gmail.com>
2023-08-15 00:03:25 -0300

    fix : correções

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,7 +6,6 @@ import { signIn, signOut, useSession } from "next-auth/react";
 import { AiOutlineMenu } from "react-icons/ai";
 import Link from "next/link";
 import Logo from "./logo";
-import NavBar from "./NavBar";
 import UserLogin from "./UserLogin";
 
 function Header(): React.JSX.Element {
@@ -24,15 +23,9 @@ function Header(): React.JSX.Element {
   return (
     <div className="container mx-auto p-5 py-0 h-[93px] flex justify-between items-center">
       <Logo />
-      <NavBar />
       {status === "unauthenticated" && <UserLogin />}
       {status === "authenticated" && data.user && (
         <div className="flex items-center p-3 gap-5  relative">
-          <AiOutlineMenu
-            size={20}
-            onClick={handleMenuClick}
-            className="cursor-pointer lg:hidden"
-          />
           <Image
             width={28}
             height={28}
@@ -41,9 +34,10 @@ function Header(): React.JSX.Element {
             className="rounded-full shadow-md cursor-pointer"
             onClick={handleMenuClick}
           />
-          <p>{data.user.name}</p>
+          <p className="hidden lg:block">{data.user.name}</p>
           {menuIsOpen && (
-            <div className=" z-50 absolute top-12  w-[110px] h-[100px] bg-white rounded-xl shadow-md flex flex-col justify-center items-center">
+            
+            <div className=" z-50 absolute top-11 right-1 w-[110px] h-[100px] bg-white rounded-xl shadow-md flex flex-col justify-center items-center">
               <Link href="/my-trips">
                 <button
                   className="text-color02 text-sm font-semibold mb-2 hover:text-color01 ease-in duration-300"
@@ -52,7 +46,6 @@ function Header(): React.JSX.Element {
                   Minhas viagens
                 </button>
               </Link>
-
               <div className="w-1/2 bg-color02 h-[1px]"></div>
               <button
                 className="text-color02 text-sm font-semibold mt-2 hover:text-color01 ease-in duration-300"
