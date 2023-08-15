4633b397acf2434fd9236d0a316c048df3f665d1
DosSantosDev2022 <dossantosdevoficial@gmail.com>
2023-08-14 23:21:42 -0300

    feat: corrigindo bugs

--- src/app/my-trips/components/UserReservationItem.tsx
+++ src/app/my-trips/components/UserReservationItem.tsx
@@ -15,21 +15,29 @@ interface UserReservationItemProps {
   fetchReservations: () => void;
 }
 
-const UserReservationItem = ({ reservation, fetchReservations }: UserReservationItemProps) => {
+const UserReservationItem = ({
+  reservation,
+  fetchReservations,
+}: UserReservationItemProps) => {
   const router = useRouter();
 
   const { trip } = reservation;
 
   const handleDeleteClick = async () => {
-    const res = await fetch(`http://localhost:3000/api/trips/reservation/${reservation.id}`, {
-      method: "DELETE",
-    });
+    const res = await fetch(
+      `http://localhost:3000/api/trips/reservation/${reservation.id}`,
+      {
+        method: "DELETE",
+      }
+    );
 
     if (!res.ok) {
       return toast.error("Ocorreu um erro ao cancelar a reserva!");
     }
 
-    toast.success("Reserva cancelada com sucesso!", { position: "bottom-center" });
+    toast.success("Reserva cancelada com sucesso!", {
+      position: "bottom-center",
+    });
 
     fetchReservations();
   };
@@ -40,7 +48,13 @@ const UserReservationItem = ({ reservation, fetchReservations }: UserReservation
       <div className="flex flex-col p-5 mt-5 border-color03 border-solid border shadow-lg rounded-lg">
         <div className="flex items-center gap-3 pb-5 border-b border-color03 border-solid">
           <div className="relative h-[106px] w-[124px]">
-            <Image src={trip.coverImage} fill style={{ objectFit: "cover" }} className="rounded-lg" alt={trip.name} />
+            <Image
+              src={trip.coverImage}
+              fill
+              style={{ objectFit: "cover" }}
+              className="rounded-lg"
+              alt={trip.name}
+            />
           </div>
 
           <div className="flex flex-col">
@@ -55,19 +69,31 @@ const UserReservationItem = ({ reservation, fetchReservations }: UserReservation
         <div className="flex flex-col mt-5 text-color02">
           <h3 className="text-sm">Data</h3>
           <div className="flex items-center gap-1">
-            <p className="text-sm">{format(new Date(reservation.startDate), "dd 'de' MMMM", { locale: ptBR })}</p>
+            <p className="text-sm">
+              {format(new Date(reservation.startDate), "dd 'de' MMMM", {
+                locale: ptBR,
+              })}
+            </p>
             {" - "}
-            <p className="text-sm">{format(new Date(reservation.endDate), "dd 'de' MMMM", { locale: ptBR })}</p>
+            <p className="text-sm">
+              {format(new Date(reservation.endDate), "dd 'de' MMMM", {
+                locale: ptBR,
+              })}
+            </p>
           </div>
 
           <h3 className="mt-5 text-sm">Hóspedes</h3>
           <p className="text-sm pb-5">{reservation.guests} hóspedes</p>
 
-          <h3 className="font-semibold text-color02 mt-3 pt-5 border-t border-solid border-color03">Informações sobre o preço</h3>
+          <h3 className="font-semibold text-color02 mt-3 pt-5 border-t border-solid border-color03">
+            Informações sobre o preço
+          </h3>
 
           <div className="flex justify-between mt-1">
             <p className="text-color02 text-sm mt-2">Total:</p>
-            <p className="font-medium text-sm">R${Number(reservation.totalPaid)}</p>
+            <p className="font-medium text-sm">
+              R${Number(reservation.totalPaid)}
+            </p>
           </div>
 
           <Button variant="danger" className="mt-5" onClick={handleDeleteClick}>
